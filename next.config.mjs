41925b7c5e3f593ff358b57e91c64ecb9dd4f2f2
Khushal Khan <khushalkhan40@gmail.com>
2024-09-04 21:42:03 +0500

    Revert "f"
    This reverts commit 2b4f72d0c10839c46de73d45b422bd5c163a8f85.

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  // Assuming the repo name is 'your-repo'
-  const repo = "http://MeanBeen.github.io/ark";
-  assetPrefix = repo;
-  basePath = repo;
-}
-
 const nextConfig = {
-  output: "export", // Enables static export
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  trailingSlash: true, // Ensures URLs end with a slash
-  images: {
-    unoptimized: true, // Disable Next.js image optimization
-  },
-  reactStrictMode: true,
-  swcMinify: true,
+  output: "export",
+  distDir: "dist",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
